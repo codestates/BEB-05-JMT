b157840f9a1b10b2463885dbc5b431ee343dc670
DevUreak <rlaquddlf3204@gmail.com>
2022-09-15 18:43:39 +0900

    0

---
+++ client/src/components/swap/Trading.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function Trading(props) {
+
+	return (
+		<div className='liquidity'>
+            토큰 스왑 
+		</div>
+	);
+}
+
+export default Trading;
--- client/src/pages/Swap.js
+++ client/src/pages/Swap.js
@@ -1,10 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { useRecoilValue, useSetRecoilState } from "recoil"
 import { backgroundAtom } from "../recoil/background/atom"
 import { accountAtom } from "../recoil/account/atom"
 
+import Trading from "../components/swap/Trading";
+import Liquidity from "../components/swap/Liquidity";
+import TokenInfo from "../components/swap/TokenInfo";
+import Typography from '@mui/material/Typography';
+import { Box, Tab, Tabs, Button } from '@mui/material';
+
 const Swap = () => {
+  const [tabIndex,setTabIndex] = useState(0);
   const account = useRecoilValue(accountAtom)
   const setBackground = useSetRecoilState(backgroundAtom)
   const navigate = useNavigate();
@@ -16,11 +23,42 @@ const Swap = () => {
     setBackground({type: 'default'});
   }, []);
 
+  const handleTabChange = (event, newTabIndex) => {
+    setTabIndex(newTabIndex);
+  };
+
 	return (
-		<div className='swap-container'>
-			스왑
-		</div>
+    <div style={{"width":1000}}>
+        <Tabs value={tabIndex} onChange={handleTabChange} variant="fullWidth">
+          <Tab label="Token Info" id='1'  />
+          <Tab label="Liquidity Manage" id='2'/>
+          <Tab label="Trading Token" id='3'/>
+        </Tabs>
+      
+  
+      {tabIndex === 0 && (
+        <TokenInfo></TokenInfo>
+      )}
+
+      {tabIndex === 1 && (
+        <Liquidity></Liquidity>
+      )}
+
+      {tabIndex === 2 && (
+        <Trading></Trading>
+      )}
+      
+      </div>
 	);
 }
 
 export default Swap;
+ // <TabPanel value={menue} index={1}>
+      //   Item One
+      // </TabPanel>
+      // <TabPanel value={menue} index={2}>
+      //   Item Two
+      // </TabPanel>
+      // <TabPanel value={menue} index={3}>
+      //   Item Three
+      // </TabPanel>
