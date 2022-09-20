a2aba9d7660b17cdd71274e8da828a965bae0907
bcdy19 <bcdy19@gmail.com>
2022-09-21 03:30:16 +0900

    fix Result

--- client/src/recoil/matching/atom.js
+++ client/src/recoil/matching/atom.js
@@ -3,11 +3,27 @@ import { recoilPersist } from 'recoil-persist'
 const { persistAtom } = recoilPersist()
 
 export const matchingAtom = atom({
-  key: 'matchingImageAtom',
+  key: 'matchingAtom',
   default: {
+    address: "",
     username: "",
-    image: "",
-    strength: ""
+    charId: "",
+    weaponId: "",
+    matchingChardata: "",
+    matchingWeapondata: "",
+    strength: "",
+    userWinImage: "",
+    userLoseImage: "",
+    MwinImage: "",
+    MloseImage: ""
+  },
+  effects_UNSTABLE: [persistAtom]
+});
+
+export const rewardScrollAtom = atom({
+  key: 'rewardScroll',
+  default: {
+    scrollId: "",
   },
   effects_UNSTABLE: [persistAtom]
 });
